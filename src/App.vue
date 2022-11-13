8b188077169773773e2038999417d2e148ec728c
ankit407 <amankit207@gmail.com>
2022-11-13 18:48:27 +0530

    covid tracker app

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <HeaderView />
+  <div class="container mx-auto">
+    <router-view />
+  </div>
+</template>
+<script>
+  import HeaderView from "@/components/Header.vue";
+
+  export default {
+    name: "AppView",
+    components: { HeaderView },
+  };
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="text-center bg-blue-500 text-white p-4 mb-10">
+    <div class="text-2xl font-bold md:text-5xl m-3">
+      <i class="fa fa-viruses"></i>
+      Covid-19 Tracker
+    </div>
+
+    <p>
+      API by
+      <a class="text-blue-300" href="https://covid19api.com" target="_blank">
+        Covid19api.com
+      </a>
+    </p>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: "HeaderView",
+  };
+</script>
